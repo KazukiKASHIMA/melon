8cd93861423ea7de10fdd5100a5c9126604ea511
Yukari Shiozaki <hw22a076@oecu.jp>
2023-10-19 14:45:44 +0900

    076

--- mac_drawpad-main/DrawPad/main.cpp
+++ mac_drawpad-main/DrawPad/main.cpp
@@ -22,6 +22,8 @@ void DrawMain()
     for (int x = 50; x < 250; x+=10) {
         Paint(x+5, 0, kColorDarkBlue, kColorBlue);
     }
+    
+    FillCircle(0, 0, 100, kColorWhite);
 
     DrawText("Hello, world!!", -12*14/2, 44, kColorWhite);
     Sleep(0.5f);
@@ -34,5 +36,6 @@ void DrawMain()
     DrawText("Hello, world!!", -12*14/2, -44, kColorRed);
     Sleep(0.5f);
     DrawText("Hello, world!!", -12*14/2, -66, kColorGray);
+    
 }
 
